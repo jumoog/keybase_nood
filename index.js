da4bf09e1dbafde87d1431ba4d82d83678a91d99
Kilian von Pflugk <github@jumoog.io>
2020-06-13 13:52:05 +0200

    use typescript

--- index.js
+++ index.js
@@ -1,87 +1,105 @@
-const Bot = require('keybase-bot')
-const bot = new Bot()
-const p = require('phin').promisified;
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const keybase_bot_1 = __importDefault(require("keybase-bot"));
+const phin_1 = __importDefault(require("phin"));
+const bot = new keybase_bot_1.default();
+const FILE_DIR = 'files/';
 async function main() {
-  try {
-    const username = process.env.KB_USERNAME
-    const paperkey = process.env.KB_PAPERKEY
-    await bot.init(username, paperkey)
-    const info = bot.myInfo()
-    console.log(`Echo bot initialized with username ${info.username}.`)
-
-    await bot.chat.clearCommands()
-    await bot.chat.advertiseCommands({
-      advertisements: [
-        {
-          type: 'public',
-          commands: [
-            {
-              name: 'catfact',
-              description: 'get random cat facts',
-              usage: 'catfact',
-            },
-          ],
-        },
-      ],
-    })
-
-    const onMessage = async message => {
-      if (message.content.type !== 'text') {
-        return
-      }
-      console.log(message.content.text.body)
-      if (message.content.text.body.startsWith('!catfact')) {
-        bot.chat.send(message.conversationId, {
-            body: await getNewCatFact()
-          })
-      }
-
-      if (message.content.text.body.includes(':wave:')){
-        bot.chat.react(message.conversationId, message.id, ':wave:')
-      }
-      if (message.content.text.body.toUpperCase().includes("HALLO")){
-        bot.chat.react(message.conversationId, message.id, ':wave:')
-      }
-      if (message.content.text.body.toUpperCase().includes("HELLO")){
-        bot.chat.react(message.conversationId, message.id, ':wave:')
-      }
-      if (message.content.text.body.toUpperCase().includes("GOOD MORNING")){
-        bot.chat.react(message.conversationId, message.id, ':wave:')
-      }
-      if (message.content.text.body.toUpperCase().startsWith("HI")){
-        bot.chat.react(message.conversationId, message.id, ':wave:')
-      }
-      if (message.content.text.body.toUpperCase().includes("BONJOUR")){
-        bot.chat.react(message.conversationId, message.id, ':wave:')
-      }
+    try {
+        const username = 'kilianbot'; // put a real username here
+        const paperkey = 'film victory pact elevator blade snap draw husband pause metal wide episode mirror'; // put a real paperkey here
+        await bot.init(username, paperkey);
+        const info = bot.myInfo();
+        console.log(`Echo bot initialized with username ${info === null || info === void 0 ? void 0 : info.username}.`);
+        await bot.chat.clearCommands();
+        await bot.chat.advertiseCommands({
+            advertisements: [
+                {
+                    type: 'public',
+                    commands: [
+                        {
+                            name: 'catfact',
+                            description: 'get random cat facts',
+                            usage: 'catfact',
+                        },
+                    ],
+                },
+            ],
+        });
+        console.log('Listening for messages...');
+        await bot.chat.watchAllChannelsForNewMessages(handleMessage, onError);
     }
-
-    const onError = e => console.error(e)
-    console.log(`Listening for messages...`)
-    await bot.chat.watchAllChannelsForNewMessages(onMessage, onError)
-  } catch (error) {
-    console.error(error)
-  }
+    catch (error) {
+        console.error(error);
+    }
 }
-
+/**
+ * exit the bot gracefully
+ */
 async function shutDown() {
-  await bot.deinit()
-  process.exit()
+    await bot.deinit();
+    process.exit();
 }
-
-process.on('SIGINT', shutDown)
-process.on('SIGTERM', shutDown)
-
-main()
-
+async function onError(err) {
+    console.error(err);
+    process.exit(1);
+}
+async function handleMessage(message) {
+    var _a, _b, _c;
+    const channel = message.channel.topicName;
+    const body = (_b = (_a = message.content) === null || _a === void 0 ? void 0 : _a.text) === null || _b === void 0 ? void 0 : _b.body;
+    if (!body) {
+        return;
+    }
+    if (message.content.type === 'attachment') {
+        if (channel === 'videos') {
+            const attachment = (_c = message.content) === null || _c === void 0 ? void 0 : _c.attachment;
+            if (!attachment) {
+                return;
+            }
+            bot.chat.download(message.conversationId, message.id, FILE_DIR + attachment.object.filename);
+            return;
+        }
+    }
+    if (body.startsWith('!catfact')) {
+        bot.chat.send(message.conversationId, {
+            body: await getNewCatFact(),
+        });
+    }
+    if (body.includes(':wave:')) {
+        bot.chat.react(message.conversationId, message.id, ':wave:');
+    }
+    if (body.toUpperCase().includes('HALLO')) {
+        bot.chat.react(message.conversationId, message.id, ':wave:');
+    }
+    if (body.toUpperCase().includes('HELLO')) {
+        bot.chat.react(message.conversationId, message.id, ':wave:');
+    }
+    if (body.toUpperCase().includes('GOOD MORNING')) {
+        bot.chat.react(message.conversationId, message.id, ':wave:');
+    }
+    if (body.toUpperCase().startsWith('HI')) {
+        bot.chat.react(message.conversationId, message.id, ':wave:');
+    }
+    if (body.toUpperCase().includes('BONJOUR')) {
+        bot.chat.react(message.conversationId, message.id, ':wave:');
+    }
+}
+process.on('SIGINT', shutDown);
+process.on('SIGTERM', shutDown);
+main();
 async function getNewCatFact() {
-    // build the URL
-    let url = `https://catfact.ninja/fact`;
-    // do the request
-    let res = await p({
-        url: url,
-        parse: 'json'
+    const url = 'https://catfact.ninja/fact';
+    const res = await phin_1.default({
+        url,
+        parse: 'json',
     });
-    return res.body.fact;
+    if (res.statusCode === 200) {
+        const catfact = res === null || res === void 0 ? void 0 : res.body;
+        return catfact.fact;
+    }
+    return res.statusCode;
 }
---
+++ index.ts
@@ -0,0 +1,116 @@
+import Bot from 'keybase-bot';
+import p from 'phin';
+import { MsgSummary } from 'keybase-bot/lib/types/chat1';
+
+const bot = new Bot();
+const FILE_DIR = 'files/';
+
+export interface Catfact {
+  fact: string;
+  length: number;
+}
+
+async function main(): Promise<void> {
+  try {
+    const username = 'kilianbot'; // put a real username here
+    const paperkey = 'film victory pact elevator blade snap draw husband pause metal wide episode mirror'; // put a real paperkey here
+    await bot.init(username, paperkey);
+    const info = bot.myInfo();
+    console.log(`Echo bot initialized with username ${info?.username}.`);
+
+    await bot.chat.clearCommands();
+    await bot.chat.advertiseCommands({
+      advertisements: [
+        {
+          type: 'public',
+          commands: [
+            {
+              name: 'catfact',
+              description: 'get random cat facts',
+              usage: 'catfact',
+            },
+          ],
+        },
+      ],
+    });
+    console.log('Listening for messages...');
+    await bot.chat.watchAllChannelsForNewMessages(handleMessage, onError);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * exit the bot gracefully
+ */
+async function shutDown(): Promise<void> {
+  await bot.deinit();
+  process.exit();
+}
+
+async function onError(err: Error): Promise<void> {
+  console.error(err);
+  process.exit(1);
+}
+
+async function handleMessage(message: MsgSummary): Promise<void> {
+  const channel = message.channel.topicName;
+  const body = message.content?.text?.body;
+  if (!body) {
+    return;
+  }
+
+  if (message.content.type === 'attachment') {
+    if (channel === 'videos') {
+      const attachment = message.content?.attachment;
+      if (!attachment) {
+        return;
+      }
+      bot.chat.download(message.conversationId, message.id, FILE_DIR + attachment.object.filename);
+      return;
+    }
+  }
+
+  if (body.startsWith('!catfact')) {
+    bot.chat.send(message.conversationId, {
+      body: await getNewCatFact(),
+    });
+  }
+
+  if (body.includes(':wave:')) {
+    bot.chat.react(message.conversationId, message.id, ':wave:');
+  }
+  if (body.toUpperCase().includes('HALLO')) {
+    bot.chat.react(message.conversationId, message.id, ':wave:');
+  }
+  if (body.toUpperCase().includes('HELLO')) {
+    bot.chat.react(message.conversationId, message.id, ':wave:');
+  }
+  if (body.toUpperCase().includes('GOOD MORNING')) {
+    bot.chat.react(message.conversationId, message.id, ':wave:');
+  }
+  if (body.toUpperCase().startsWith('HI')) {
+    bot.chat.react(message.conversationId, message.id, ':wave:');
+  }
+  if (body.toUpperCase().includes('BONJOUR')) {
+    bot.chat.react(message.conversationId, message.id, ':wave:');
+  }
+}
+
+process.on('SIGINT', shutDown);
+process.on('SIGTERM', shutDown);
+
+main();
+
+async function getNewCatFact(): Promise<string> {
+  const url = 'https://catfact.ninja/fact';
+  const res: any = await p({
+    url,
+    parse: 'json',
+  });
+  if (res.statusCode === 200) {
+    const catfact: Catfact = res?.body;
+    return catfact.fact;
+  }
+  return res.statusCode;
+}
